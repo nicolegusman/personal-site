4d1eb2c7eb95c3fd752fed40756f58b24b291eec
NewNico <NewNico@NG-Laptop-2023.local>
2026-07-17 13:52:03 -0700

    Create reusable page container component

---
+++ content/home.ts
@@ -0,0 +1,8 @@
+export const home = {
+  title: "Nico Gusman",
+
+  subtitle: "User Experience Researcher",
+
+  introduction:
+    "Helping organizations better understand people so they can make better decisions about the products, services, and systems they create.",
+};
